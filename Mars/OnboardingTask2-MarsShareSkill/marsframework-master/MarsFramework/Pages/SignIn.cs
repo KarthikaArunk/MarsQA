7a0963fa72a61f2a2db5af7afea14ff28d5a4310
karthika <karthika@arunkumar.cloud>
2022-02-16 19:29:26 +1000

    first commit

---
+++ Mars/OnboardingTask2-MarsShareSkill/marsframework-master/MarsFramework/Pages/SignIn.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using System;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace MarsFramework.Pages
+{
+    class SignIn
+    {
+        public SignIn()
+        {
+            PageFactory.InitElements(Global.GlobalDefinitions.driver, this);
+        }
+
+        #region  Initialize Web Elements 
+        //Finding the Sign Link
+        [FindsBy(How = How.XPath, Using = "//a[contains(text(),'Sign In')]")]
+        private IWebElement SignIntab { get; set; }
+
+        // Finding the Email Field
+        [FindsBy(How = How.Name, Using = "email")]
+        private IWebElement Email { get; set; }
+
+        //Finding the Password Field
+        [FindsBy(How = How.Name, Using = "password")]
+        private IWebElement Password { get; set; }
+
+        //Finding the Login Button
+        [FindsBy(How = How.XPath, Using = "//button[contains(text(),'Login')]")]
+        private IWebElement LoginBtn { get; set; }
+
+        #endregion
+
+        internal void LoginSteps()
+        {
+            Global.GlobalDefinitions.driver.Navigate().GoToUrl("http://localhost:5000");
+            SignIntab.Click();
+            Email.SendKeys("karthika9@gmail.com");
+            Password.SendKeys("P@ssword");
+            LoginBtn.Click();
+        }
+    }
+}
---
+++ Mars/OnboardingTask2-MarsShareSkill/marsframework-master/MarsFramework/Pages/SignInPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.PageObjects;
+
+
+namespace MarsFramework.Pages
+{
+    public class SignInPage
+    {
+        // IWebDriver driver;
+        ////Finding the SignIn Button
+        //[FindsBy(How =How.LinkText,Using ="Sign In")]
+        //private IWebElement signInBtn { get; set; }
+        
+        
+        
+    }
+}
